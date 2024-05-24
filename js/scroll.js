446c45e2fc937541450e7302bb4a666b5b5c0c60
Pedro <pedroliveiralima21@gmail.com>
2024-05-24 19:28:25 -0300

    adicionando animação scroll

---
+++ js/scroll.js
@@ -0,0 +1,17 @@
+window.sr = ScrollReveal({ reset: true });
+
+sr.reveal(".main-content h1 ", { duration: 1000 });
+sr.reveal(".main-content p", { delay: 300 });
+sr.reveal(".btn-FC", { delay: 300 });
+
+sr.reveal(".about-description01 h1", { duration: 1000 });
+sr.reveal(".about-description02 h1", { duration: 500 });
+sr.reveal(".about-description01 p", { delay: 300 });
+sr.reveal(".about-description02 p", { delay: 300 });
+sr.reveal(".about-img", { delay: 300 });
+
+sr.reveal(".services-cards", { delay: 300 });
+sr.reveal(".customers-title", { delay: 400 });
+sr.reveal(".customers-logo img", { duration: 1000 });
+sr.reveal(".form-text", { duration: 2000 });
+sr.reveal("form" , {duration: 1000})
